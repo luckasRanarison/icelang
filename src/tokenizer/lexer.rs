41f95c7ba2ca4411d769497ea31c4a70c37ecda8
Luckas <luckasranarison@gmail.com>
2023-04-05 11:41:33 +0300

    feat: functional basic lexer

--- src/tokenizer/lexer.rs
+++ src/tokenizer/lexer.rs
@@ -24,6 +24,15 @@ impl<'a> Lexer<'a> {
         while let Some(ch) = self.chars.next() {
             self.current_pos.end = self.current_pos.start;
 
+            if ch == '-' {
+                if let Some(next_char) = self.chars.peek() {
+                    if *next_char == '-' {
+                        self.skip_comment();
+                        continue;
+                    }
+                }
+            }
+
             if ch.is_whitespace() {
                 self.current_pos.start = self.current_pos.end + 1;
 
@@ -37,15 +46,21 @@ impl<'a> Lexer<'a> {
 
             self.current_lexeme += &ch.to_string();
 
-            if is_standard_symbol(ch) {
-                let token = self.create_symbol_token();
-                tokens.push(token);
-            }
+            let token = match ch {
+                ch if is_standard_symbol(ch) => self.create_symbol_token(),
+                ch if is_quote(ch) => self.create_string_token(ch),
+                ch if is_alphabetic(ch) => self.create_keyword_or_identifer_token(),
+                ch if ch.is_ascii_digit() => self.create_number_token(),
+                _ => Token::new(TokenType::Invalid, String::new(), self.current_pos),
+            };
+
+            tokens.push(token);
 
             self.current_lexeme = String::new();
             self.current_pos.start = self.current_pos.end + 1;
         }
 
+        self.current_pos.end = self.current_pos.start;
         let eof_token = Token::new(TokenType::Eof, String::new(), self.current_pos);
         tokens.push(eof_token);
 
@@ -57,6 +72,16 @@ impl<'a> Lexer<'a> {
         self.current_pos.end += 1;
     }
 
+    fn skip_comment(&mut self) {
+        while let Some(ch) = self.chars.next() {
+            if ch == '\n' {
+                self.current_pos.line += 1;
+                self.current_pos.start = 0;
+                break;
+            }
+        }
+    }
+
     fn create_symbol_token(&mut self) -> Token {
         if let Some(next_char) = self.chars.peek() {
             if is_standard_symbol(*next_char) {
@@ -65,8 +90,7 @@ impl<'a> Lexer<'a> {
             }
         }
 
-        let lexeme = self.current_lexeme.clone();
-        let token_type = match lexeme.as_str() {
+        let token_type = match self.current_lexeme.as_str() {
             "+" => TokenType::Plus,
             "-" => TokenType::Minus,
             "*" => TokenType::Asterix,
@@ -89,7 +113,93 @@ impl<'a> Lexer<'a> {
             _ => TokenType::Invalid,
         };
 
-        let token = Token::new(token_type, lexeme, self.current_pos);
+        let token = Token::new(token_type, self.current_lexeme.clone(), self.current_pos);
+
+        token
+    }
+
+    fn create_string_token(&mut self, quote_char: char) -> Token {
+        let mut closed = false;
+
+        while let Some(next_char) = self.chars.peek() {
+            if *next_char == '\n' {
+                break;
+            } else if *next_char == quote_char {
+                closed = true;
+                self.current_lexeme += &next_char.to_string();
+                self.advance();
+                break;
+            } else {
+                self.current_lexeme += &next_char.to_string();
+                self.advance();
+            }
+        }
+
+        if !closed {
+            panic!("Unmatched quote")
+        }
+
+        let value = self.current_lexeme[1..self.current_lexeme.len() - 1].to_string();
+        let token = Token::new(
+            TokenType::String(value),
+            self.current_lexeme.clone(),
+            self.current_pos,
+        );
+
+        token
+    }
+
+    fn create_number_token(&mut self) -> Token {
+        while let Some(next_char) = self.chars.peek() {
+            if next_char.is_ascii_digit() || *next_char == '.' {
+                self.current_lexeme += &next_char.to_string();
+                self.advance();
+            } else {
+                break;
+            }
+        }
+
+        let token = match self.current_lexeme.parse::<f64>() {
+            Ok(value) => Token::new(
+                TokenType::Number(value),
+                self.current_lexeme.clone(),
+                self.current_pos,
+            ),
+            Err(_) => {
+                panic!("Invalid number value")
+            }
+        };
+
+        token
+    }
+
+    fn create_keyword_or_identifer_token(&mut self) -> Token {
+        while let Some(ch) = self.chars.peek() {
+            if is_alphanumeric(*ch) {
+                self.current_lexeme += &ch.to_string();
+                self.advance();
+            } else {
+                break;
+            }
+        }
+
+        let token_type = match self.current_lexeme.as_str() {
+            "var" => TokenType::Var,
+            "true" => TokenType::True,
+            "false" => TokenType::False,
+            "null" => TokenType::Null,
+            "and" => TokenType::And,
+            "or" => TokenType::Or,
+            "if" => TokenType::If,
+            "else" => TokenType::Else,
+            "for" => TokenType::For,
+            "while" => TokenType::While,
+            "fun" => TokenType::Fun,
+            "return" => TokenType::Return,
+            _ => TokenType::Identifier(self.current_lexeme.clone()),
+        };
+
+        let token = Token::new(token_type, self.current_lexeme.clone(), self.current_pos);
 
         token
     }
--- src/tokenizer/utils.rs
+++ src/tokenizer/utils.rs
@@ -2,3 +2,15 @@ pub fn is_standard_symbol(ch: char) -> bool {
     let symbols = "+-*/(){}.,;!<>=";
     symbols.contains(ch)
 }
+
+pub fn is_quote(ch: char) -> bool {
+    ch == '\"' || ch == '\''
+}
+
+pub fn is_alphabetic(ch: char) -> bool {
+    ch.is_ascii_alphabetic() || ch == '_'
+}
+
+pub fn is_alphanumeric(ch: char) -> bool {
+    ch.is_ascii_alphanumeric() || ch == '_'
+}
